ca7a584b7f1d84b347f259e308f7ac02c253019b
윤철중 <cjfwnd123zz@gmail.com>
2021-09-11 00:47:37 +0900

    add: 내가 과거 제출한 기분 불러오기 추가

--- src/api/routes/mood.js
+++ src/api/routes/mood.js
@@ -15,7 +15,7 @@ function moodRouter(root) {
   router.get("/", auth.isLogin, async (req, res, next) => {
     try {
       const { id } = req.user;
-      const mood = await MoodService.getMoodByUser(id);
+      const mood = await MoodService.getMoodToday(id);
 
       res.status(200).json(getApi({ suc: true, data: mood }));
     } catch (err) {
@@ -31,11 +31,27 @@ function moodRouter(root) {
     try {
       const { id } = req.user;
       const { temperature, description } = req.body;
-      const mood = await MoodService.getMoodByUser(id);
+      const mood = await MoodService.getMoodToday(id);
       if (mood) return res.status(200).json(getApi({ suc: true, data: mood, mes: "오늘 기분 이미 제출" }));
 
       const newMood = await MoodService.postMood(id, temperature, description);
       res.status(201).json(getApi({ suc: true, data: newMood, mes: "오늘 기분 제출 완료" }));
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  /**
+   * @description 내가 제출한 기분 조회
+   * @route GET /mood/:moodId
+   */
+  router.get("/:moodId", auth.isLogin, async (req, res, next) => {
+    try {
+      const { id } = req.user;
+      const { moodId } = req.params;
+      const mood = await MoodService.getMoodByUser(id, moodId);
+
+      res.status(200).json(getApi({ suc: true, data: mood }));
     } catch (err) {
       next(err);
     }
--- src/services/mood.js
+++ src/services/mood.js
@@ -1,9 +1,10 @@
 import moment from "moment";
 import Sequelize from "sequelize";
 import Mood from "../models/Mood";
+import CustomError from "../utils/customError";
 
 const MoodService = {
-  getMoodByUser: async (userId) => {
+  getMoodToday: async (userId) => {
     const mood = await todayMoodByUser(userId);
     return mood;
   },
@@ -12,6 +13,12 @@ const MoodService = {
     const newMood = await Mood.create({ temperature, description, userId });
     return newMood;
   },
+
+  getMoodByUser: async (userId, moodId) => {
+    const mood = await Mood.findOne({ where: { id: moodId, userId } });
+    if (!mood) throw new CustomError("EXIST_NOT_MOOD", 404, "제출된 기분이 없습니다.");
+    return mood;
+  },
 };
 
 async function todayMoodByUser(userId) {
@@ -19,6 +26,7 @@ async function todayMoodByUser(userId) {
   const TODAY = moment().format("YYYY-MM-DD");
   const TOMORROW = moment().add(1, "d").format("YYYY-MM-DD");
   const mood = await Mood.findOne({ where: { userId, createdAt: { [Op.gt]: TODAY, [Op.lt]: TOMORROW } } });
+  if (!mood) throw new CustomError("EXIST_NOT_MOOD", 404, "제출된 기분이 없습니다.");
   return mood;
 }
 
