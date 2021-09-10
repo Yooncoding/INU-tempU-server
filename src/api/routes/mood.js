9f34bb9384812515f5ca4232789ae968427a11d0
윤철중 <cjfwnd123zz@gmail.com>
2021-09-10 23:21:20 +0900

    add: 오늘 기분 제출하기 추가

--- src/api/routes/mood.js
+++ src/api/routes/mood.js
@@ -22,6 +22,24 @@ function moodRouter(root) {
       next(err);
     }
   });
+
+  /**
+   * @description 오늘 기분 제출
+   * @route POST /mood
+   */
+  router.post("/", auth.isLogin, async (req, res, next) => {
+    try {
+      const { id } = req.user;
+      const { temperature, description } = req.body;
+      const mood = await MoodService.getMoodByUser(id);
+      if (mood) return res.status(200).json(getApi({ suc: true, data: mood, mes: "오늘 기분 이미 제출" }));
+
+      const newMood = await MoodService.postMood(id, temperature, description);
+      res.status(201).json(getApi({ suc: true, data: newMood, mes: "오늘 기분 제출 완료" }));
+    } catch (err) {
+      next(err);
+    }
+  });
 }
 
 export default moodRouter;
--- src/services/mood.js
+++ src/services/mood.js
@@ -4,17 +4,22 @@ import Mood from "../models/Mood";
 
 const MoodService = {
   getMoodByUser: async (userId) => {
-    const Op = Sequelize.Op;
-    const TODAY = moment().format("YYYY-MM-DD");
-    const TOMORROW = moment().add(1, "d").format("YYYY-MM-DD");
-    const mood = await Mood.findOne({
-      where: {
-        userId,
-        createdAt: { [Op.gt]: TODAY, [Op.lt]: TOMORROW },
-      },
-    });
+    const mood = await todayMoodByUser(userId);
     return mood;
   },
+
+  postMood: async (userId, temperature, description) => {
+    const newMood = await Mood.create({ temperature, description, userId });
+    return newMood;
+  },
 };
+
+async function todayMoodByUser(userId) {
+  const Op = Sequelize.Op;
+  const TODAY = moment().format("YYYY-MM-DD");
+  const TOMORROW = moment().add(1, "d").format("YYYY-MM-DD");
+  const mood = await Mood.findOne({ where: { userId, createdAt: { [Op.gt]: TODAY, [Op.lt]: TOMORROW } } });
+  return mood;
+}
 
 export default MoodService;
